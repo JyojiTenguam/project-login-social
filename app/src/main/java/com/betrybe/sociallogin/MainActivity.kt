04f6d05dddce7cbd6ca9cb30a997c1d6ac2eb0c0
JyojiTenguam <j.y.oji@hotmail.com>
2024-11-03 02:09:23 -0300

    'Resolvendo requisito 08 de Project login social'

--- app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
+++ app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
@@ -5,10 +5,13 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var emailInputLayout: TextInputLayout
     private lateinit var emailInput: EditText
     private lateinit var passwordInput: EditText
     private lateinit var loginButton: Button
@@ -20,11 +23,21 @@ class MainActivity : AppCompatActivity() {
         emailInput = findViewById(R.id.email_input)
         passwordInput = findViewById(R.id.password_input)
         loginButton = findViewById(R.id.login_button)
+        emailInputLayout = findViewById(R.id.email_text_input_layout)
 
         loginButton.isEnabled = false
 
         emailInput.addTextChangedListener(textWatcher)
         passwordInput.addTextChangedListener(textWatcher)
+
+        loginButton.setOnClickListener {
+            if (isValidEmail(emailInput.text.toString())) {
+                emailInputLayout.error = null
+                Toast.makeText(this, "Login realizado com sucesso", Toast.LENGTH_SHORT).show()
+            } else {
+                emailInputLayout.error = "Email inválido"
+            }
+        }
     }
 
     private val textWatcher = object : TextWatcher {
@@ -37,5 +50,10 @@ class MainActivity : AppCompatActivity() {
         }
 
         override fun afterTextChanged(string: Editable?) {}
+    }
+
+    private fun isValidEmail(email: String): Boolean {
+        val emailPattern = "[a-zA-Z0-9.]+@[a-zA-Z]+\\.[a-zA-Z]+"
+        return email.matches(emailPattern.toRegex())
     }
 }
