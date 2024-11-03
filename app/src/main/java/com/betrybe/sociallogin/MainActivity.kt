7677203cfe70dc40e94c6820a34a39badcb1833d
JyojiTenguam <j.y.oji@hotmail.com>
2024-11-03 05:35:39 -0300

    'style: atualizar cores do projeto e feat: mudar cor do botão ativado'

--- app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
+++ app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.betrybe.sociallogin
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -8,6 +10,7 @@ import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputLayout
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,13 +24,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Define a cor verde #1EB620 para a status bar
+        window.statusBarColor = ContextCompat.getColor(this, R.color.status_bar_green)
+
         emailInputLayout = findViewById(R.id.email_text_input_layout)
         passwordInputLayout = findViewById(R.id.password_text_input_layout)
         emailInput = findViewById(R.id.email_input)
         passwordInput = findViewById(R.id.password_input)
         loginButton = findViewById(R.id.login_button)
 
+        // Desativar o botão inicialmente
         loginButton.isEnabled = false
+        loginButton.backgroundTintList = ColorStateList.valueOf(Color.parseColor("#DCDEDA")) // Fundo cinza claro
+        loginButton.setTextColor(Color.parseColor("#919191")) // Texto cinza
 
         emailInput.addTextChangedListener(textWatcher)
         passwordInput.addTextChangedListener(textWatcher)
@@ -62,6 +71,15 @@ class MainActivity : AppCompatActivity() {
             val isEmailFilled = emailInput.text.toString().isNotEmpty()
             val isPasswordFilled = passwordInput.text.toString().isNotEmpty()
             loginButton.isEnabled = isEmailFilled && isPasswordFilled
+
+            // Alterar a aparência do botão ao ativá-lo
+            if (loginButton.isEnabled) {
+                loginButton.backgroundTintList = ColorStateList.valueOf(Color.parseColor("#A8DAB5")) // Fundo verde claro
+                loginButton.setTextColor(Color.parseColor("#005700")) // Texto verde escuro
+            } else {
+                loginButton.backgroundTintList = ColorStateList.valueOf(Color.parseColor("#DCDEDA")) // Fundo cinza claro
+                loginButton.setTextColor(Color.parseColor("#919191")) // Texto cinza
+            }
         }
 
         override fun afterTextChanged(string: Editable?) {}
